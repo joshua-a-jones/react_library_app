16800fb9ca1035b1dae14400e4b8db323b89a12d
Joshua Jones <joshua.desjones@gmail.com>
2020-07-26 14:45:16 -0500

    Updates

---
+++ precache-manifest.630f1c00046ffdc87fc1c67b5e121f25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5952b34a6d5491354edd638572f72fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccc1978806680035c430",
+    "url": "/static/css/main.c673d5ac.chunk.css"
+  },
+  {
+    "revision": "dc3f1c571b568cfe81f2",
+    "url": "/static/js/2.a6421908.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6421908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc1978806680035c430",
+    "url": "/static/js/main.a6e5f9d4.chunk.js"
+  },
+  {
+    "revision": "b93a299d4bfbc679cad4",
+    "url": "/static/js/runtime-main.e10e84d2.js"
+  }
+]);
